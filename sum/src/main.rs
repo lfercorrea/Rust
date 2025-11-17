5537fa254c51ecf74db82a33adf24e8520fcfdd7
nando correa <lfercorrea@gmail.com>
2025-11-17 05:56:22 -0300

    refactored code

--- sum/src/main.rs
+++ sum/src/main.rs
@@ -1,15 +1,21 @@
 fn main() {
     let numbers = "10, 20, 30, 40, 50, 60, 70, 80, 90, 100";
+    let obj_numbers = split_numbers(numbers);
+    let mut sum: i32 = 0;
+    println!("Targets: \x1b[1;32m{:?}\x1b[0m", obj_numbers);
+    for i in obj_numbers {
+        sum += i;
+    }
+    println!("The sum is: \x1b[1;32m{sum}\x1b[0m")
+}
 
-    let obj_numbers: Vec<i32> = numbers
+fn split_numbers(s: &str) -> Vec<i32> {
+    let obj_numbers: Vec<i32> = s
         .split(",")
         .map(|s| s.trim())
         .filter(|s| !s.is_empty())
-        .map(|s| s.parse::<i32>().unwrap())
+        .filter_map(|s| s.parse::<i32>().ok())
         .collect();
 
-    // println!("{:?}", obj_numbers);
-    for i in obj_numbers {
-        println!("{i}")
-    }
+    obj_numbers
 }
