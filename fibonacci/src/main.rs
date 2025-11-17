5b614e1aff0a0a7a0f03e7b04c7a3e7fb5ea7e8e
nando correa <lfercorrea@gmail.com>
2025-11-17 00:01:40 -0300

    fibonacci added

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,14 +1,26 @@
 fn main() {
-
-    println!("{fibonacci}");
+    for i in 0..20 {
+        let fib = fibonacci(i);
+        println!("{fib}")
+    }
 }
 
-fn fibonacci(i64 num)
-{
-    if num < 2 {
-        return num;
+fn fibonacci(n: i64) -> i64 {
+    if n < 2 {
+        return n;
     }
 
-    let mut n: i32 = 1;
+    let mut a: i64 = 0;
+    let mut b: i64 = 1;
+    let mut i: i64 = 2;
     let mut fib: i64 = 1;
+
+    while i <= n {
+        fib = a + b;
+        a = b;
+        b = fib;
+        i += 1;
+    }
+
+    fib
 }
