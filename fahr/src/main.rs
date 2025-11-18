c2cb76eff80b07914f4b02bfdc9b3a8bbe6f158a
nando correa <lfercorrea@gmail.com>
2025-11-17 21:09:05 -0300

    input improvement

--- fahr/src/main.rs
+++ fahr/src/main.rs
@@ -1,14 +1,14 @@
 use std::io;
 
 fn main() {
-    println!("Type a temp in Celsius:");
+    let msg: &str = "Type a temperature in °C to convert to °F: ";
+    println!("{msg}");
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
-        .expect("Failed to read input");
+        .expect("Failed obtaining number from stdin");
 
-    let celsius: f64 = input.trim().parse().expect("CONVERSION INPUT FAILURE");
+    let celsius: f64 = input.trim().parse::<f64>().expect("Conversion failure");
     let fahr: f64 = celsius * 9.0 / 5.0 + 32.0;
-
     println!("{celsius}°C = {fahr}°F");
 }
