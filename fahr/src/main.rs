38fab0eac796dc81de87f032b81b6e63c0d678b4
nando correa <lfercorrea@gmail.com>
2025-12-01 07:04:25 -0300

    .

--- fahr/src/main.rs
+++ fahr/src/main.rs
@@ -1,14 +1,27 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
-    let msg: &str = "Type a temperature in °C to convert to °F: ";
-    println!("{msg}");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed obtaining number from stdin");
-
-    let celsius: f64 = input.trim().parse::<f64>().expect("Conversion failure");
-    let fahr: f64 = celsius * 9.0 / 5.0 + 32.0;
-    println!("{celsius}°C = {fahr}°F");
+    loop {
+        let mut input = String::new();
+        print!("Type a temp in Celsius to be converted to Fahrenheight: ");
+        match io::stdout().flush() {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Error printing stdout: {e}");
+            }
+        }
+        io::stdin().read_line(&mut input).unwrap();
+        if input.trim() == "exit" {
+            break;
+        }
+        let celsius: f64 = match input.trim().parse() {
+            Ok(v) => v,
+            Err(e) => {
+                println!("Error converting from std to f64: {e}");
+                continue;
+            }
+        };
+        let fahr: f64 = celsius * 9.0 / 5.0 + 32.0;
+        println!("{}°C = {}°F", celsius, fahr);
+    }
 }
