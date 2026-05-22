dbd19615aceb40bc3948ba0d0e5fe286b534b429
nando correa <lfercorrea@gmail.com>
2026-05-22 08:53:55 -0300

    modified String to PathBuf in Args fields

--- claps_training/src/main.rs
+++ claps_training/src/main.rs
@@ -2,22 +2,37 @@ use clap::Parser;
 use std::{
     fs::File,
     io::{Read, Write},
+    path::{Path, PathBuf},
 };
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    infile: String,
+    infile: PathBuf,
     #[arg(short, long)]
-    outfile: String,
+    outfile: PathBuf,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let mut infile = File::open(args.infile).unwrap();
-    let mut outfile = File::create(args.outfile).unwrap();
+    let mut infile = File::open(&args.infile).unwrap();
+    let mut outfile = File::create(&args.outfile).unwrap();
+
+    println!(
+        "File: {}",
+        &args.infile.file_stem().unwrap().to_string_lossy()
+    );
+    println!(
+        "Extension: {}",
+        &args.infile.extension().unwrap().to_string_lossy()
+    );
+    println!(
+        "Filename {}.{}",
+        &args.infile.file_stem().unwrap().to_string_lossy(),
+        &args.infile.extension().unwrap().to_string_lossy()
+    );
 
     let mut buf = Vec::new();
 
