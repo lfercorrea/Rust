1a1a0c9419014300b36a74f106eafb8afa3aae30
nando correa <lfercorrea@gmail.com>
2026-05-27 11:33:36 -0300

    m

--- matrix/src/main.rs
+++ matrix/src/main.rs
@@ -1,28 +1,55 @@
 use myrustlib::get_i32;
 use std::io;
 
-fn main() {
-    println!("Type matrix rows: ");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let lines: usize = input.trim().parse().unwrap();
+struct Matrix(Vec<i64>);
 
-    println!("Type matrix cols: ");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let cols: usize = input.trim().parse().unwrap();
+impl Matrix {
+    fn new(rows: usize, cols: usize) -> Self {
+        let mut matrix = vec![0; rows * cols];
+        // let matrix: Vec<Vec<i64>> = (0..rows).map(|_| (0..cols).map(|_| 0).collect()).collect();
 
-    let mut matrix: Vec<Vec<i64>> = Vec::new();
-    // let matrix: Vec<Vec<i64>> = (0..lines).map(|_| (0..cols).map(|_| 0).collect()).collect();
-
-    for _ in 0..lines {
-        let mut line: Vec<i64> = Vec::new();
-        for _ in 0..cols {
-            let n = get_i32("Type the matrix item:");
-            line.push(n as i64);
+        for x in 0..rows {
+            for y in 0..cols {
+                let n = get_i32("Type next matrix element: ");
+                matrix[x * cols + y] = n as i64;
+            }
         }
-        matrix.push(line);
+
+        Self(matrix)
     }
 
-    println!("matrix: {:?}", matrix)
+    fn print_mtx(&self, height: usize, width: usize) {
+        let mut l_edge = true;
+        for i in 0..height {
+            for j in 0..width {
+                if l_edge {
+                    print!("|\t");
+                    l_edge = false;
+                }
+
+                print!("{}", self.0[i * width + j]);
+
+                if j > 0 || j < width {
+                    print!("\t")
+                }
+
+                if j == width - 1 {
+                    println!("|");
+                    l_edge = true;
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let rows = get_i32("Tipe matrix rows: ");
+    let rows = rows as usize;
+    let cols = get_i32("Tipe matrix cols: ");
+    let cols = cols as usize;
+
+    let matrix = Matrix::new(rows, cols);
+
+    // println!("matrix: {:?}", matrix.0)
+    matrix.print_mtx(rows, cols);
 }
