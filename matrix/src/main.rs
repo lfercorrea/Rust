5c7abdc8d90969fc4c3f2ee24743b822b4b3be06
nando correa <lfercorrea@gmail.com>
2026-05-27 21:18:44 -0300

    m

--- matrix/src/main.rs
+++ matrix/src/main.rs
@@ -1,7 +1,10 @@
 use myrustlib::get_i32;
-use std::io;
 
-struct Matrix(Vec<i64>);
+struct Matrix {
+    data: Vec<i64>,
+    height: usize,
+    width: usize,
+}
 
 impl Matrix {
     fn new(rows: usize, cols: usize) -> Self {
@@ -15,10 +18,59 @@ impl Matrix {
             }
         }
 
-        Self(matrix)
+        Self {
+            data: matrix,
+            height: rows,
+            width: cols,
+        }
     }
 
-    fn print_mtx(&self, height: usize, width: usize) {
+    fn sum(&self, rhs_mtx: Matrix) -> Self {
+        assert!(
+            self.height == rhs_mtx.height && self.width == rhs_mtx.width,
+            "The matrixes has incompatible dimensions to sum together."
+        );
+        let mut new_mtx = vec![0; rhs_mtx.height * rhs_mtx.width];
+
+        for i in 0..self.height {
+            for j in 0..self.width {
+                new_mtx[i * self.width + j] =
+                    self.data[i * self.width + j] + rhs_mtx.data[i * self.width + j];
+            }
+        }
+
+        Self {
+            data: new_mtx,
+            height: self.height,
+            width: self.width,
+        }
+    }
+
+    fn mul(&self, rhs_mtx: Matrix) -> Self {
+        assert!(
+            self.width == rhs_mtx.height,
+            "The matrixes haven't compatible dimensions to perform a multiplication."
+        );
+
+        let mut new_mtx = vec![0; self.height * rhs_mtx.width];
+
+        for i in 0..self.height {
+            for j in 0..rhs_mtx.width {
+                for k in 0..self.width {
+                    new_mtx[i * rhs_mtx.width + j] +=
+                        self.data[i * self.width + k] * rhs_mtx.data[k * rhs_mtx.width + j];
+                }
+            }
+        }
+
+        Self {
+            data: new_mtx,
+            height: self.height,
+            width: rhs_mtx.width,
+        }
+    }
+
+    fn print(&self, height: usize, width: usize) {
         let mut l_edge = true;
         for i in 0..height {
             for j in 0..width {
@@ -27,7 +79,7 @@ impl Matrix {
                     l_edge = false;
                 }
 
-                print!("{}", self.0[i * width + j]);
+                print!("{}", self.data[i * width + j]);
 
                 if j > 0 || j < width {
                     print!("\t")
@@ -43,13 +95,25 @@ impl Matrix {
 }
 
 fn main() {
-    let rows = get_i32("Tipe matrix rows: ");
-    let rows = rows as usize;
-    let cols = get_i32("Tipe matrix cols: ");
-    let cols = cols as usize;
+    // let rows = get_i32("Tipe matrix rows: ");
+    // let rows = rows as usize;
+    // let cols = get_i32("Tipe matrix cols: ");
+    // let cols = cols as usize;
 
-    let matrix = Matrix::new(rows, cols);
+    // let matrix = Matrix::new(rows, cols);
 
-    // println!("matrix: {:?}", matrix.0)
-    matrix.print_mtx(rows, cols);
+    let mtx_a = Matrix {
+        data: vec![2, 3, 4, 1, 0, 0],
+        height: 2,
+        width: 3,
+    };
+    let mtx_b = Matrix {
+        data: vec![0, 1000, 1, 100, 0, 10],
+        height: 3,
+        width: 2,
+    };
+
+    let mtx_c = mtx_a.mul(mtx_b);
+
+    mtx_c.print(2, 2);
 }
